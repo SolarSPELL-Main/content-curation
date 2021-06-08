dbdbd3bfed33d0bc785178f279b404a5fb259a94
Pavithra Devi <65974841+prajkuma@users.noreply.github.com>
2021-06-08 12:09:42 -0700

    validators new file
    Validators → Validates the files.

---
+++ content_curation/validators.py
@@ -0,0 +1,23 @@
+import os
+import filecmp
+from django.core.exceptions import ValidationError
+from django.utils.text import get_valid_filename
+
+import settings
+
+def validate_unique_filename(value):
+    filepath = os.path.join(settings.CONTENTS_ROOT, get_valid_filename(value.name))
+    if os.path.isfile(filepath):
+        raise ValidationError('Filename already exists.')
+
+def validate_unique_file(value):
+    ##Yet to find the use
+    from utils import sha256
+    files = [
+        os.path.join(settings.CONTENTS_ROOT, filename) for filename
+        in os.listdir(os.path.join(settings.CONTENTS_ROOT))
+    ]
+    for f in files:
+        if os.path.getsize(f) == value.size:
+            if sha256(open(f, "rb")) == sha256(value.file):
+                raise ValidationError('File already exists with name ' + os.path.basename(f))
