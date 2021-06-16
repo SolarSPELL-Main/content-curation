6832f9ec4c36e315e567999c0e60581b84feb2e9
Pavithra Devi <65974841+prajkuma@users.noreply.github.com>
2021-06-15 17:17:46 -0700

    Update urls.py
    Router for metadata,metadatatype

--- backend/urls.py
+++ backend/urls.py
@@ -1,9 +1,17 @@
 from django.urls import include, path
 from rest_framework import routers
 from django.conf.urls.static import static
+from rest_framework import routers
+from .views import mock_data,MetadataTypeViewSet,MetadataViewSet
 
-from .views import mock_data
+router = routers.DefaultRouter()
+router.register(r'metadata', MetadataViewSet,basename='metadata')
+router.register(r'metadata_types', MetadataTypeViewSet,basename='metadata_types')
 
 urlpatterns = [
-    path('metadata', mock_data),
+    #path(r'metadata', mock_data),
+    path('', include(router.urls)),
+    #path(r'metadata',MetadataViewSet.as_view({'get':'get'}),name="metadata"),
+    path(r'metadata',MetadataViewSet.as_view,name="metadata"),
 ]
+
