c39d9c8ccb8ef29be26bc49e3718972aeeef6e5a
Pavithra Devi <65974841+prajkuma@users.noreply.github.com>
2021-06-15 17:16:21 -0700

    Standardize Format , Serializers
    Standardize Format , Serializers for metadata metadatatype

---
+++ backend/standardize_format.py
@@ -0,0 +1,21 @@
+from rest_framework.views import exception_handler
+from rest_framework import status
+from rest_framework.response import Response
+
+
+def standard_exception_handler(exc, context):
+    response = exception_handler(exc, context)
+    if response is not None:
+        response.data = {
+            'data': None,
+            'error': getattr(response, "data", ""),
+            'success': False
+        }
+    return response
+
+def build_response(data=None, status=status.HTTP_200_OK, success=True, error=None):
+    return Response({
+        "success": success,
+        "data": data,
+        "error": error
+    }, status)
