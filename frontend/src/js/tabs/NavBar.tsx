a3010f9118c009fb5d3311e74a49b464af383ad6
Jonathan Zhao <zhaojonathan99@gmail.com>
2021-06-17 11:51:14 -0700

    Added global slice for current tab

--- frontend/src/js/tabs/NavBar.tsx
+++ frontend/src/js/tabs/NavBar.tsx
@@ -3,6 +3,9 @@ import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
 import { Link as RouterLink, useLocation } from 'react-router-dom';
 
+import { useCCSelector, useCCDispatch } from '../hooks';
+import { update_current_tab } from '../state/global';
+
 import Logo from '../../assets/logo.png';
 
 type NavBarProps = {
@@ -15,6 +18,12 @@ const logoStyle: React.CSSProperties = {
     margin: '10px',
 };
 
+const logoTabStyle: React.CSSProperties = {
+    width: '320px',
+    maxWidth: '320px',
+    backgroundColor: 'var(--ocean-blue)',
+};
+
 const linkMap: Record<string, number> = {
     '/': 0,
     '/home': 0,
@@ -24,11 +33,18 @@ const linkMap: Record<string, number> = {
 
 function NavBar(_: NavBarProps): React.ReactElement {
     const path = useLocation().pathname;
-    const [currentTab, setCurrentTab] = React.useState(linkMap[path]);
+    const currentTab = useCCSelector(state => state.global.current_tab);
+    const dispatch = useCCDispatch();
+
+    const updateTab = React.useCallback((tab: number) => {
+        dispatch(update_current_tab(tab));
+    }, [dispatch]);
 
     React.useEffect(() => {
-        setCurrentTab(linkMap[path]);
-    }, [setCurrentTab, path]);
+        if (linkMap[path] !== currentTab) {
+            updateTab(linkMap[path]);
+        }
+    }, [updateTab, currentTab, path]);
 
     return (
         <Tabs
@@ -36,9 +52,9 @@ function NavBar(_: NavBarProps): React.ReactElement {
             TabIndicatorProps={{style: {backgroundColor: '#75B2DD', height: '5px', borderRadius: '5px'}}}
             indicatorColor={'primary'}
             variant={'scrollable'}
-            onChange={(_, v) => setCurrentTab(v)}
+            onChange={(_, v) => updateTab(v)}
         >
-            <Tab component={RouterLink} style={{ width: '320px', maxWidth: '320px', backgroundColor: 'var(--ocean-blue)' }} to={'/home'} label={<img src={Logo} style={logoStyle} />} />
+            <Tab component={RouterLink} style={logoTabStyle} to={'/home'} label={<img src={Logo} style={logoStyle} />} />
             <Tab component={RouterLink} to={'/metadata'} label={'Metadata'} />
             <Tab component={RouterLink} to={'/content'} label={'Content'} />
         </Tabs>
