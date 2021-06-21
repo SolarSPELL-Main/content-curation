7655e85e09424fe2bb9b6543f6a5996ccf49a434
Pavithra Devi <65974841+prajkuma@users.noreply.github.com>
2021-06-21 16:41:57 -0700

    update serializer file
    For metadata/metadatatype CRUD operations

--- backend/serializers.py
+++ backend/serializers.py
@@ -3,16 +3,15 @@ from rest_framework.validators import UniqueTogetherValidator
 from backend.models import Metadata, MetadataType
 
 
-#serializer : Converts native to python and vice versa
 class MetadataTypeSerializer(ModelSerializer):
     class Meta:
         model = MetadataType
         #fields = '__all__'
         fields=("id","name")
 
-#helps with validation of fields
+
 class MetadataSerializer(ModelSerializer):
-    type = MetadataTypeSerializer()
+    #type = MetadataTypeSerializer()
     class Meta:
         validators = [
             UniqueTogetherValidator(
@@ -22,23 +21,4 @@ class MetadataSerializer(ModelSerializer):
         ]
         model = Metadata
         fields = ('id', 'name', 'type')
-    """class Meta:
-        model=MetadataType
-        fields=("id","name","type")"""
-
-    #overrides in-built update operation
-    def update(self,instance, validated_data):
-        print("Serializer Update metadata")
-        metadata_validated_data = validated_data.pop('name')
-        metadata = instance.metadata
-
-        #save metadata info
-        instance.name=validated_data.get('name',instance.name)
-        instance.save()
-
-        #save metadatatype info
-        metadata.type=metadata_validated_data.get('type',instance.type)
-        metadata.save()
-
-        return instance
-
+    
