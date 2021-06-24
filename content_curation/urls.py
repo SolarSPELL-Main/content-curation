8a5e3d0a135d4b67d82e1208a5fb49b90f664fd3
Pavithra Devi <65974841+prajkuma@users.noreply.github.com>
2021-06-23 18:34:54 -0700

    update urls.py google OAuth login

--- content_curation/urls.py
+++ content_curation/urls.py
@@ -15,9 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from backend.views import Welcome
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('backend.urls')),
+    path('accounts/', include('allauth.urls')),
+    path('', Welcome.as_view(), name='welcome'),
 ]
