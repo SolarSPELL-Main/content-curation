3c8f975bc209680afad16cdee8c7a6633f5a3510
Pavithra Devi <65974841+prajkuma@users.noreply.github.com>
2021-06-25 16:41:05 -0700

    access token login
    gets access token from the view page

--- backend/urls.py
+++ backend/urls.py
@@ -2,7 +2,7 @@ from django.urls import include, path
 from rest_framework import routers
 from django.conf.urls.static import static
 from rest_framework import routers
-from .views import mock_data,MetadataTypeViewSet,MetadataViewSet
+from .views import mock_data,MetadataTypeViewSet,MetadataViewSet,welcome
 
 router = routers.DefaultRouter()
 router.register(r'metadata', MetadataViewSet,basename='metadata')
@@ -11,5 +11,6 @@ router.register(r'metadata_types', MetadataTypeViewSet,
 
 urlpatterns = [
     #path(r'metadata', mock_data),
+    path(r'welcome', welcome),
     path('', include(router.urls)),
 ]
