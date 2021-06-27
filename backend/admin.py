459a7fd6132755ec87723dbfd5c95c46cb87be02
Pavithra Devi <65974841+prajkuma@users.noreply.github.com>
2021-06-26 21:23:24 -0700

    Registered Models to admin file
    Registered Models to the Admin File

--- backend/admin.py
+++ backend/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import MetadataType, Metadata, Content
 # Register your models here.
+admin.site.register(MetadataType)
+admin.site.register(Metadata)
+admin.site.register(Content)
