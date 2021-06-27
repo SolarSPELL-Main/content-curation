ae3d406494daf07461d2218f61feab1a8787e0e0
Pavithra Devi <65974841+prajkuma@users.noreply.github.com>
2021-06-26 21:25:02 -0700

    Added Content api url
    added Content api url

--- backend/urls.py
+++ backend/urls.py
@@ -2,12 +2,14 @@ from django.urls import include, path
 from rest_framework import routers
 from django.conf.urls.static import static
 from rest_framework import routers
-from .views import mock_data,MetadataTypeViewSet,MetadataViewSet,welcome
+from .views import mock_data, MetadataTypeViewSet, MetadataViewSet, \
+    ContentViewSet, welcome
 
 router = routers.DefaultRouter()
 router.register(r'metadata', MetadataViewSet,basename='metadata')
 router.register(r'metadata_types', MetadataTypeViewSet,
                 basename='metadata_types')
+router.register(r'content', ContentViewSet, basename='content')
 
 urlpatterns = [
     #path(r'metadata', mock_data),
