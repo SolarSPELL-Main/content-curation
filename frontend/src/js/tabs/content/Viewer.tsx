6e7a018ce3eb3f5e105320c94ddc37d1f6283727
Jonathan Zhao <zhaojonathan99@gmail.com>
2021-07-09 17:28:43 -0700

    Added view content

---
+++ frontend/src/js/tabs/content/Viewer.tsx
@@ -0,0 +1,78 @@
+import React from 'react';
+
+import { ContentViewer } from 'solarspell-react-lib';
+import { MetadataType, Content } from 'js/types';
+
+type ViewerProps = {
+    content: Content
+    metadataTypes: MetadataType[]
+    open: boolean
+    onClose: () => void
+}
+
+/**
+ * Boilerplate implementation of ContentViewer.
+ * @param props Context and callbacks of content view.
+ * @returns A dialog displaying information about a single piece of content.
+ */
+function Viewer(props: ViewerProps): React.ReactElement {
+    return (
+        <ContentViewer
+            {...props}
+            items={[
+                {
+                    title: 'Title',
+                    field: 'title',
+                },
+                {
+                    title: 'Description',
+                    field: 'description',
+                },
+                {
+                    title: 'Filename',
+                    field: 'fileName',
+                },
+                {
+                    title: 'Created On',
+                    field: 'createdDate',
+                },
+                {
+                    title: 'Year of Publication',
+                    field: 'datePublished',
+                },
+                {
+                    title: 'Copyright Approved',
+                    field: 'copyrightApproved',
+                    formatter: (b: boolean) => b ? 'Yes' : 'No',
+                },
+                {
+                    title: 'Copyright Notes',
+                    field: 'copyright',
+                },
+                {
+                    title: 'Rights Statement',
+                    field: 'rightsStatement',
+                },
+                {
+                    title: 'Additional Notes',
+                    field: 'notes',
+                },
+            ]}
+            fileDisplay={{
+                field: 'fileURL',
+                // Currently unused
+                formatter: (_url: string) => (
+                    <object
+                        width={600}
+                        height={600}
+                        data={new URL('https://image.shutterstock.com/image-vector/smile-icon-vector-face-emoticon-260nw-1721368459.jpg').href}
+                    >
+                        File
+                    </object>
+                )
+            }}
+        />
+    )
+}
+
+export default Viewer;
