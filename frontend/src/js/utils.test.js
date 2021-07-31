f3aa73076d7a73c08bdc9813af35f3a02dd1791b
rishabhskp <65858680+rishabhskp@users.noreply.github.com>
2021-07-30 19:08:28 -0700

    Getting started with unit testing
    installed "jest" to get started with unit testing

---
+++ frontend/src/js/utils.test.js
@@ -0,0 +1,7 @@
+const { updateCRUDPermissions } = require('./util');
+
+test('should output name and age', () => {
+    const text = generateText('Max', 29);
+    expect(text).toBe('Max (29 years old)');
+    
+});
