5ac18910e4b8b04f39604a9f747f20310b672e0e
Jonathan Zhao <41343736+Jonathanzhao02@users.noreply.github.com>
2021-08-03 15:51:56 -0700

    Refactored content/metadata tabs (#55)
    * Refactored metadata tab
    
    * Fixed comment
    
    * Started refactor for content tab
    
    * Finished content tab refactor
    
    * Fixed missing dialog close
    
    * Changed comment for clarification
    
    * Renamed modal -> body
    
    * Removed min/max review dates

---
+++ frontend/src/js/pages/content/SelectedToolbar/DeleteSelected.tsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+
+import { ConfirmationDialog } from 'solarspell-react-lib';
+import * as Actions from '../../../state/content';
+import { useCCDispatch, useCCSelector } from '../../../hooks';
+
+/**
+ * This is the 'Delete' button that shows below the search bar when the user
+ * has selected content.
+ * It prompts the user if they are sure they want to delete selected content.
+ * If agreed, the user's selected content will all be deleted.
+ * @returns A button to delete selected members of the content table.
+ */
+function DeleteSelected(): React.ReactElement {
+    const dispatch = useCCDispatch();
+    const selected = useCCSelector(state => state.content.selected);
+    const [open, setOpen] = React.useState(false);
+
+    return (
+        <>
+            <ConfirmationDialog
+                open={open}
+                // Plural shown for when # items != 1
+                title={`Delete ${selected.length} selected ${
+                    selected.length == 1 ? 'item' : 'items'
+                }?`}
+                size={'xs'}
+                onClose={agreed => {
+                    // On agree, dispatch action to delete all selected content
+                    if (agreed) {
+                        // To avoid dealing with pages that no longer exist,
+                        // reset user back to first page.
+                        dispatch(Actions.update_pagination({
+                            page: 0,
+                        }));
+                        dispatch(Actions.delete_content(selected));
+                    }
+                    setOpen(false);
+                }}
+            />
+            <Button
+                variant={'contained'}
+                color={'secondary'}
+                onClick={() => setOpen(true)}
+            >
+                Delete
+            </Button>
+        </>
+    );
+}
+
+export default DeleteSelected;
--- frontend/src/js/pages/content/SelectedToolbar/SelectedToolbar.tsx
+++ frontend/src/js/pages/content/SelectedToolbar/SelectedToolbar.tsx
@@ -2,56 +2,38 @@ import React from 'react';
 import Box from '@material-ui/core/Box';
 import Collapse from '@material-ui/core/Collapse';
 
-import ShowForPermission from '../ShowForPermission';
+import { useCCSelector } from '../../../hooks';
+import ShowForPermission from '../../ShowForPermission';
 import DeleteSelected from './DeleteSelected';
 import ClearSelected from './ClearSelected';
 import ExportSelected from './ExportSelected';
 
-type SelectedToolbarActions = {
-    onDelete: (ids: number[]) => void
-    onClear: (ids: number[]) => void
-    onExport: (ids: number[]) => void
-}
-
-type SelectedToolbarProps = {
-    selected: number[]
-    actions: SelectedToolbarActions
-}
-
 /**
  * This is the toolbar that appears whenever the user makes a selection in
  * the DataGrid.
+ * It appears only if the user has selected any content using the checkboxes
+ * present in the content table.
  * Collapse animation to reduce jarring appearance/disappearance of toolbar.
  * @param props Context and callbacks of the toolbar. 
  * @returns An animated toolbar.
  */
-function SelectedToolbar({
-    selected,
-    actions,
-}: SelectedToolbarProps): React.ReactElement {
+function SelectedToolbar(): React.ReactElement {
+    const selected = useCCSelector(state => state.content.selected);
+
     return (
         <Collapse in={selected.length > 0}>
             <Box mb={2} mt={2} display={'flex'}>
                 <Box mr={2} >
-                    <ClearSelected
-                        selected={selected}
-                        onClear={actions.onClear}
-                    />
+                    <ClearSelected />
                 </Box>
                 <ShowForPermission slice={'content'} permission={'delete'}>
                     <Box mr={2} >
-                        <DeleteSelected
-                            selected={selected}
-                            onDelete={actions.onDelete}
-                        />
+                        <DeleteSelected />
                     </Box>
                 </ShowForPermission>
                 <ShowForPermission slice={"special"} permission={"export"}>
                     <Box mr={2} >
-                        <ExportSelected
-                            selected={selected}
-                            onExport={actions.onExport}
-                        />
+                        <ExportSelected />
                     </Box>
                 </ShowForPermission>
             </Box>
@@ -59,5 +41,4 @@ function SelectedToolbar({
     );
 }
 
-export { SelectedToolbarActions };
 export default SelectedToolbar;
