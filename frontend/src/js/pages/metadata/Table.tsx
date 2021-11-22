a28cf79742cc33558258acb056fab842fbe0bccc
Zain Jakwani <zjakwani@gmail.com>
2021-11-22 10:38:32 -0700

    Integrate client side pagination w/ metadata table

--- frontend/src/js/pages/metadata/Table.tsx
+++ frontend/src/js/pages/metadata/Table.tsx
@@ -1,14 +1,13 @@
-import React from 'react';
-
 import type {
-  GridSortModel,
+  GridSortModel
 } from '@material-ui/data-grid';
-
+import React from 'react';
 import { MetadataDisplay } from 'solarspell-react-lib';
-import { useCCSelector } from '../../hooks';
+import { useCCDispatch, useCCSelector } from '../../hooks';
+import * as MetadataActions from '../../state/metadata';
 import { hasPermission } from '../../utils/permissions';
-import KebabMenu from './KebabMenu';
 import ActionPanel from './ActionPanel';
+import KebabMenu from './KebabMenu';
 
 /**
  * This component is the table that displays all metadata types and their
@@ -19,8 +18,12 @@ import ActionPanel from './ActionPanel';
 function Table(): React.ReactElement {
   const metadata = useCCSelector(state => state.metadata.metadata);
   const metadataTypes = useCCSelector(state => state.metadata.metadata_types);
+  const dispatch = useCCDispatch();
+  const page = (id: number) => useCCSelector(state => state.metadata.metadata_page[id]);
+  const pageSize = (id: number) => useCCSelector(state => state.metadata.metadata_pagesize[id]);
 
-  const [pageSize, setPageSize] = React.useState(5);
+
+  //const [pageSize, setPageSize] = React.useState(5);
   // Default sort metadata by name
   const [sortModel, setSortModel] = React.useState<GridSortModel>([{
     field: 'name',
@@ -47,14 +50,18 @@ function Table(): React.ReactElement {
           ActionPanel: showActionPanel ? ActionPanel : undefined,
         },
         additionalProps: {
-          pageSize: pageSize,
-          onPageSizeChange: params => setPageSize(params.pageSize),
           rowsPerPageOptions: [5, 10, 25],
           sortModel: sortModel,
           onSortModelChange: params => 
             setSortModel(params.sortModel),
         },
         mountContents: false,
+      }}
+      paginationProps={{
+        pageSize: pageSize,
+        page: page,
+        update: MetadataActions.update_pagination,
+        dispatch: dispatch,
       }}
     />
   );
