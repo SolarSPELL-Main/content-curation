889aca22868f54c5830a986b930f8b4cb7cdb8d8
Pavithra Devi <65974841+prajkuma@users.noreply.github.com>
2022-03-30 14:05:48 -0700

    Logger in log file

--- backend/standardize_format.py
+++ backend/standardize_format.py
@@ -1,11 +1,15 @@
 from rest_framework.views import exception_handler
 from rest_framework import status
 from rest_framework.response import Response
+import logging
 
+logger = logging.getLogger(__name__)
 
 def standard_exception_handler(exc, context):
+   
     response = exception_handler(exc, context)
     if response is not None:
+        logger.error(getattr(response, "data", ""))
         response.data = {
             'data': None,
             'error': getattr(response, "data", ""),
@@ -15,6 +19,7 @@ def standard_exception_handler(exc, context):
 
 def build_response(data=None, status=status.HTTP_200_OK, success=True, 
                     error=None):
+    logger.warning(data)
     return Response({
         "success": success,
         "data": data,
